d614707e264c533327c5b92c430141a990f08f30
shinhee <tlsgml656@gmail.com>
2022-12-26 13:21:26 +0900

    빈배열 선언 및 저장된 값 확인

--- todos/src/components/TodoList.vue
+++ todos/src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul>
-      <li class="shadow">할일</li>
+      <li class="shadow"></li>
       <li>할일</li>
       <li>할일</li>
     </ul>
@@ -10,7 +10,19 @@
 
 <script>
 export default {
-
+  data(){
+    return{
+    // Storage 내용을 넣을 빈 배열 생성
+    todoItems:[]
+    }
+  },
+  created: function(){
+    if(localStorage.length>0){
+      for(let i = 0; i < localStorage.length; i++){
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  }
 }
 </script>
 
