38a68bd6771cd889c7e414756015424cf52d4360
shinhee <tlsgml656@gmail.com>
2022-12-26 12:41:07 +0900

    로컬스토리지 저장

--- todos/src/components/TodoInput.vue
+++ todos/src/components/TodoInput.vue
@@ -1,15 +1,37 @@
 <template>
-  <div>
-    <input type="text">입력창
+  <div class="">
+    <input type="text" placeholder="Please enter a task to do." v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <span v-on:click="addTodo">
+      <i class="fa-solid fa-plus"></i>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      newTodoItem:''
+    }
+  },
+  methods: {
+    addTodo(){
+      // localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      // console.log('클릭', this.newTodoItem);
+      if(this.newTodoItem !== ''){
+        let value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value,value);
+        this.clearInput(); // 분리 단일 책임 원칙
+      }
+    },
+    clearInput(){
+      // 인풋박스 입력 후 초기화
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  
 </style>
